84167da44233026bccf4c106b37c147235b78d8e
bocchi <boccfiap@outlook.com>
2026-09-11 22:08:17 -0300

    Sprint 3: cria array de rotas mapeando URL -> Controller/método

---
+++ config/routes.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Controllers\EnergeticoController;
+
+return [
+    'criar' => [EnergeticoController::class, 'criar'],
+    'store' => [EnergeticoController::class, 'store'],
+    'editar' => [EnergeticoController::class, 'editar'],
+    'update' => [EnergeticoController::class, 'update'],
+    'listar' => [EnergeticoController::class, 'listar'],
+    'deletar' => [EnergeticoController::class, 'deletar'],
+
+];
